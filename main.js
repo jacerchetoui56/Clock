4c2dccde3b13eb895a2693514a3008d9513e7627
jacerchetoui56 <77737324+jacerchetoui56@users.noreply.github.com>
2022-06-04 05:23:36 +0100

    adding the main files

---
+++ main.js
@@ -0,0 +1,16 @@
+const secondsHand = document.querySelector('.seconds-hand');
+const minutesHand = document.querySelector('.minutes-hand');
+const hoursHand = document.querySelector('.hours-hand');
+
+function updateTime() {
+    let now = new Date()
+    let seconds = now.getSeconds()
+    let minutes = now.getMinutes()
+    let hours = now.getHours()
+
+    secondsHand.style.transform = `rotate(${seconds * 6}deg)`
+    minutesHand.style.transform = `rotate(${minutes * 6}deg)`
+    hoursHand.style.transform = `rotate(${hours * 30}deg)`
+}
+
+let interval = setInterval(updateTime, 1000)
